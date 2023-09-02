03065e18b7a245632fa21f85c3593e10871ef67a
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-09-02 11:05:55 +0545

    handle error when guess cant be parsed

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,18 @@ use rand::{Rng, thread_rng};
 fn main() {
     let random = thread_rng().gen_range(1..=10);
 
-    println!("Enter your guess from 1 to 10: ");
-
     loop {
+        println!("Enter your guess from 1 to 10: ");
+
         let mut guess = String::new();
         stdin().read_line(&mut guess).expect("failed to read");
-        let guess_num: i32 = guess.trim().parse().expect("invalid number");
+        let guess_num: i32 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Invalid number");
+                continue;
+            }
+        };
 
         match guess_num.cmp(&random) {
             Ordering::Less => println!("too small"),
